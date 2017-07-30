2f3b47a09bc0b5514522deabdc56c47da11b3880
lsf1111 <lsf111122>
2017-07-30 17:59:33 +0400

    Initial commit

---
+++ app/src/main/java/am/ysu/ysum/MainActivity.java
@@ -0,0 +1,31 @@
+package am.ysu.ysum;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.myalpha);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.startAnimation(animation);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent i3 = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i3);
+                finish();
+            }
+        }, 3000);
+    }
+}
